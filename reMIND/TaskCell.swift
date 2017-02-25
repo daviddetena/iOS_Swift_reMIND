6fa0817f209ad925668c35527e090651db5071c4
David de Tena <daviddetenacalvo@gmail.com>
2017-02-25 22:50:43 +0100

    Included logic for saving and loading tasks from disk, and appending new task to task list

---
+++ reMIND/TaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCell.swift
+//  reMIND
+//
+//  Created by David de Tena on 25/02/2017.
+//  Copyright © 2017 David de Tena. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet weak var imgTask : UIImageView!
+    @IBOutlet weak var lblTask:  UILabel!
+}
